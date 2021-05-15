799396b58e9eb96ffb1478ea6ffbca2c8f929951
hans <hans.krebs@me.com>
2021-05-15 18:30:00 +0200

    Update work experience and education

--- content/education.js
+++ content/education.js
@@ -1,6 +1,23 @@
 const educationData = [
     ['from', 'to', 'what'],
-    ['something', 'to come', 'really really soon']
+    ['01.10.2012', '10.08.2019',
+        [
+            'Bachelor Degree in Computer Science',
+            '',
+            'Part-time bachelor\'s study at the long-distance university of Hagen',
+            'Final degree with a grade point average of 1.2'
+        ]
+    ],
+    ['March 2015', 'until', 'Certified ScrumMaster'],
+    ['01.03.2006', '28.02.2010',
+        [
+            'Bachelor Degree in Industrial Engineering',
+            '',
+            'Full-time bachelor\'s study at the university of applied sciences of Esslingen',
+            'University award for the best final degree of the graduation year',
+            'Final degree with a grade point average of 1.4'
+        ]
+    ]
 ]
 
 export { educationData }
--- content/work-experience.js
+++ content/work-experience.js
@@ -1,8 +1,30 @@
 const workExperienceData = [
     ['from', 'to', 'what'],
-    ['01.01.2020', 'until', ['Lead Architect at Mercedes-Benz AG', '', 'Technical and architectural responsibility for system landscape supplying region Europe with marketing and sales-relevant vehicle data']],
-    ['01.10.2016', '31.12.2019', ['Solutions Architect at Daimler AG', '', 'Design and architecture new Daimler applications and being team\'s responsibile for architecture & tech']],
-    ['31.12.1999', '31.12.1999', ['Year 2k Bug Fixer']]
+    ['01.01.2020', 'until',
+        ['Lead Architect at Mercedes-Benz AG',
+            '', 'Technical and architectural responsibility for system landscape supplying region Europe with marketing and sales-relevant vehicle data'
+        ]
+    ],
+    ['01.10.2016', '31.12.2019',
+        ['Solutions Architect at Daimler AG',
+            '', 'Design and architecture new Daimler applications and being team\'s responsibile for architecture & tech'
+        ]
+    ],
+    ['01.01.2016', '30.09.2016',
+        ['Deployment Manager at Daimler TSS',
+            '', 'Built an automated deployment pipeline to stabilize highly-interdependent & multi-tech product deployments and optimized technical dependencies'
+        ]
+    ],
+    ['01.12.2014', '31.12.2015',
+        ['Java Software Developer at Icon Systemhaus GmbH',
+            '', 'Design, development, programming and testing of new Java modules as well as their integration into the overall system. Supervision and further development of a Scrum team as a Scrum Master'
+        ]
+    ],
+    ['01.07.2010', '31.03.2013',
+        ['Software Sales Manager at ATOSS Software and Mindlab Solutions',
+            '', 'Management of software, pilot and PoC projects for acquisition of existing and new customer projects'
+        ]
+    ]
 ];
 
 export { workExperienceData }
