b6971f737645fc4f357eac1ca433bbd7f32c3d3e
hans <hans.krebs@me.com>
2021-05-15 16:22:29 +0200

    Make headers as terminal out

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ const renderTables = () => {
 }
 
 document.querySelector('#resume').addEventListener('click', () => {
-  let resumeBox = new WinBox(winboxOptions('My Resume', '#resume-content', renderTables))
+  let resumeBox = new WinBox(winboxOptions('', '#resume-content', renderTables))
   window.winbox = resumeBox
   renderTables()
 })
 
 document.querySelector('#contact').addEventListener('click', () => {
-  let contactBox = new WinBox(winboxOptions('Contact Me', '#contact-content'))
+  let contactBox = new WinBox(winboxOptions('', '#contact-content'))
   window.winbox = contactBox
 })
 
