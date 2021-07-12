da954c6f70e1bc67fcce694dad3ce3c4940c4a81
hans <hans.krebs@me.com>
2021-07-12 17:03:05 +0200

    Make changes to work-experience wording.

--- content/work-experience.js
+++ content/work-experience.js
@@ -3,15 +3,14 @@ const workExperienceData = [
     ['01.01.2020', 'until',
         ['Lead Architect at Mercedes-Benz AG',
             '', '° Technical and architectural responsibility for system landscape supplying region Europe with marketing and sales-relevant vehicle data',
-            '° work closely with other software and systems architects as well as business owners, ops, information security officers, management and other stakeholders to lay out and drive the design of software architectures for the system landscape supplying region Europe with marketing and sales-relevant vehicle data of Mercedes-Benz',
-            '° make high-level design and architecture choices (microservices, interfaces, components) based on business and non-functional requirements',
-            '° enforce technical and security standards',
-            '° Analyse and assess technical and architectural fit of existing and future systems',
-            '° Create and evaluate concepts and scenarios for reducing redundancy, sharpening domain driven design, increase technical excellence',
+            '° Work closely with other software and systems architects as well as business owners, ops, information security officers, management and other stakeholders to lay out and drive the design of software architectures for the system landscape supplying region Europe with marketing and sales-relevant vehicle data of Mercedes-Benz',
+            '° Define target architecture and APIs, make high-level design and architecture decisions (microservices, interfaces, components) based on business and non-functional requirements',
+            '° Enforce technical and security standards',
+            '° Assess technical and architectural fit of existing and future systems',
+            '° Create and evaluate concepts and scenarios for reducing redundancy, foster domain driven design, increase technical excellence',
             '° Create and evaluate scenarios for technical rollouts and migrations',
             '° Support senior management and decision makers with technical expertise',
             '° Collaborate with other data supplying and consuming systems, as well as technical platform providers and strategic architecture initiatives',
-            '° Foster knowledge built-up within the teams, challenge status-quo and guide colleagues'
         ]
     ],
     ['01.10.2016', '31.12.2019',
@@ -22,7 +21,7 @@ const workExperienceData = [
     ['01.01.2016', '30.09.2016',
         ['Deployment Manager at Daimler TSS',
             '', '° Build an automated deployment pipeline to stabilize highly-interdependent & multi-tech product deployments',
-            '° Optimize technical dependencies'
+            '° Optimize and reduce technical dependencies'
         ]
     ],
     ['01.12.2014', '31.12.2015',
@@ -33,9 +32,9 @@ const workExperienceData = [
     ],
     ['01.07.2010', '31.03.2013',
         ['Software Sales Manager at ATOSS Software and Mindlab Solutions',
-            '', 'Management of software, pilot and PoC projects for acquisition of existing and new customer projects'
+            '', 'Manage pilot and PoC projects for acquisition of existing and new customer projects'
         ]
-    ]
+    ],
 ];
 
 export { workExperienceData }
